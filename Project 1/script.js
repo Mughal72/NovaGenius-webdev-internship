173d93ba224061186eb51dce7458596b0d24cb5e
Anber sattar <82819709+Mughal72@users.noreply.github.com>
2024-07-04 12:28:19 +0500

    Create script.js

---
+++ Project 1/script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dotsTrack = document.querySelector("#slider-buttons");
+  const dots = Array.from(dotsTrack.children);
+  let activeDot = dots[0];
+  const carousel = document.querySelector(".slider-carousel");
+  const slideCards = Array.from(carousel.querySelectorAll(".card"));
+
+  // let slideWidth=card.getBoundingClientRect().width;
+
+  const movetoSlide = (track, currentSlide, targetSlide, leftValue) => {
+    track.style.transform = "translateX(-" + leftValue + ")";
+    console.log(targetSlide.style.left);
+    currentSlide.classList.remove("current-card");
+    targetSlide.classList.add("current-card");
+  };
+
+  dotsTrack.addEventListener("click", (e) => {
+    const targetDot = e.target.closest("button");
+    if (targetDot) {
+    }
+    const currentSlide = carousel.querySelector(".current-card");
+    const currentdot = dotsTrack.querySelector(".active");
+    const dotIndex = dots.findIndex((dot) => dot === targetDot);
+    const targetSlide = slideCards[dotIndex];
+    const computedStyle = window.getComputedStyle(targetSlide);
+    const leftValue = computedStyle.getPropertyValue("left");
+    movetoSlide(carousel, currentSlide, targetSlide, leftValue);
+  });
+
+  for (let circle of dots) {
+    circle.addEventListener("click", function () {
+      activeDot.classList.remove("active");
+      activeDot = circle;
+      activeDot.classList.add("active");
+    });
+  }
+
+  document
+    .querySelector('input[type="submit"]')
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+});
